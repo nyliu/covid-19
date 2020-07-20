c3e8d5f144d642f0c98ee253a3dbd4ec88d6a5d2
Naiyou <nyliu80@gmail.com>
2020-07-20 16:07:19 -0500

    Updates

---
+++ precache-manifest.5622d31d2b5c6d888428b421bd4d1a0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01a3232a8c37bd4a395645ed5f3d2860",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "f1928bb4f0ffe88a53f6",
+    "url": "/covid-19-tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b7190c0d2a38c35e0ee",
+    "url": "/covid-19-tracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f1928bb4f0ffe88a53f6",
+    "url": "/covid-19-tracker/static/js/2.77cbe6fc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19-tracker/static/js/2.77cbe6fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7190c0d2a38c35e0ee",
+    "url": "/covid-19-tracker/static/js/main.a2e55c4e.chunk.js"
+  },
+  {
+    "revision": "fb403f6422f9b7442ea3",
+    "url": "/covid-19-tracker/static/js/runtime-main.960a8408.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid-19/precache-manifest.32c8ed4c6fd97900089cb0e5f0521634.js"
+  "/covid-19-tracker/precache-manifest.5622d31d2b5c6d888428b421bd4d1a0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid-19-tracker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
